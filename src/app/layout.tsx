5781078890c26b0ab699c11c0db4200ed1367107
duclt3005 <duclt3005@gmail.com>
2023-11-27 08:32:09 +0700

    config package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
+import '@/assets/styles/app.scss';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.scss';
-
-const inter = Inter({ subsets: ['latin'] });
+import { UIProviders } from './ui-providers';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body>
+        <UIProviders>{children}</UIProviders>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,49 @@
+'use client';
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import Select from 'react-select';
+
 export default function Home() {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
   return (
     <main>
-      <h1>Hello</h1>
+      <div className='container'>
+        <h1>Hello</h1>
+        <button>Hello</button>
+        <br />
+        <a href=''>Hello</a>
+        <div className='test'>
+          <input type='text' placeholder='Hello' className='cu-input' />
+          <Select
+            instanceId='lang'
+            options={options}
+            classNamePrefix='cu-select'
+          />
+        </div>
+        <br />
+        <Select
+          defaultValue={[options[1], options[2]]}
+          isMulti
+          name='colors'
+          options={options}
+          className='basic-multi-select'
+          classNamePrefix='cu-select'
+          instanceId='lang'
+        />
+        <Menu>
+          <MenuButton as={Button}>Actions</MenuButton>
+          <MenuList>
+            <MenuItem>Download</MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
+      </div>
     </main>
   );
 }
